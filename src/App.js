9fd2ae09de60c50640e615635c156228e9279278
SerePate <serepate@umail.iu.edu>
2017-07-06 16:27:46 -0400

    sync notes with firebase

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import base from './base'
 
 
 import './App.css'
@@ -13,7 +14,16 @@ class App extends Component {
       currentNote: this.blankNote(),
     }
   }
+  componentDidMount = () => {
+    base.syncState(
+      'notes',
+      {
+        context: this, //what object the state is on
+        state: 'notes', // which property to sync
 
+      }
+    )
+  }
 
   blankNote = () => {
     return {
@@ -44,7 +54,7 @@ class App extends Component {
 
   removeCurrentNote = () => {
     const notes = {...this.state.notes}
-    delete notes[this.state.currentNote.id]
+    notes[this.state.currentNote.id] = null
 
     this.setState({ notes })
     this.resetCurrentNote()
