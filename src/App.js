d97dd9cd85c7be6411c5641ea945f4fc9aa741d3
SerePate <serepate@umail.iu.edu>
2017-07-06 11:25:41 -0400

    key props. App.js edit

--- src/App.js
+++ src/App.js
@@ -3,13 +3,33 @@ import './App.css'
 import Main from './Main'
 
 class App extends Component {
+  constructor(){
+    super()
+    
+    this.state = {
+      notes: {
+        'note-4': {
+        id: 'note-4',
+        title: 'My fancy note',
+        body: 'Oh so fancy'
+      },
+      'note-5': {
+        id: 'note-5',
+        title: 'Another one',
+        body: 'Also very fancy',
+      },
+    }
+  }
+}
+
   render() {
     return (
     <div className="App">
-      <Main />
+      <Main note = {this.state.notes} />
     </div>
     )
   }
 }
+
 
 export default App
--- src/NoteList.js
+++ src/NoteList.js
@@ -3,32 +3,17 @@ import React from 'react'
 import Note from './Note'
 import './NoteList.css'
 
-const NoteList=()=>{
-  const notes = {
-     'note-4': {
-      id: 'note-4',
-      title: 'My fancy note',
-      body: 'Oh so fancy'
-    },
-    'note-5': {
-      id: 'note-5',
-      title: 'Another one',
-      body: 'Also very fancy',
-    }
-
-  }
-
-  const noteIds = Object.keys(notes)
+const NoteList=(props)=>{
+  const noteIds = Object.keys(props.notes)
 
     return(
         <div className="NoteList">
           <h3>Notes</h3>
           <ul id="notes">
-            {noteIds.map(noteId=> <Note notes={noteID}/> )}
+            {noteIds.map(noteId=> 
+              <Note notes={props.notes[noteId]}/> )}
           </ul>
-        </div>
-
-        
+        </div>        
     )
 }
 
